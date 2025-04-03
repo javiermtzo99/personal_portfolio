3cdf80aeeaabbf86ee172a1bb28d522cdad1ea1e
Javier Martinez <javiermtz99@icloud.com>
2025-04-03 12:02:33 -0700

    Fix active class for Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
 
-export default function Header() {
+export default function Header({ currentPath }) {
+  function getClassNameFor(path) {
+    return currentPath === path ? 'header-section active' : 'header-section';
+  }
   return (
     <>
       {/* Mobile Header */}
@@ -19,36 +22,46 @@ export default function Header() {
 
       {/* Desktop Header */}
       <div className="site-header">
-        <div className="header-section active">
-          <div className="header-icon">
-            <i className="fa-solid fa-address-card"></i>
+        <a href="/about">
+          <div className={getClassNameFor('/about')}>
+            <div className="header-icon">
+              <i className="fa-solid fa-address-card"></i>
+            </div>
+            <p>About</p>
           </div>
-          <p>About</p>
-        </div>
-        <div className="header-section">
-          <div className="header-icon">
-            <i className="fa-solid fa-school"></i>
+        </a>
+        <a href="/education">
+          <div className={getClassNameFor('/education')}>
+            <div className="header-icon">
+              <i className="fa-solid fa-school"></i>
+            </div>
+            <p>Education</p>
           </div>
-          <p>Education</p>
-        </div>
-        <div className="header-section">
-          <div className="header-icon">
-            <i className="fa-solid fa-briefcase"></i>
+        </a>
+        <a href="/work_experience">
+          <div className={getClassNameFor('/work_experience')}>
+            <div className="header-icon">
+              <i className="fa-solid fa-briefcase"></i>
+            </div>
+            <p>Work Exp</p>
           </div>
-          <p>Work Exp</p>
-        </div>
-        <div className="header-section">
-          <div className="header-icon">
-            <i className="fa-solid fa-code"></i>
+        </a>
+        <a href="/projects">
+          <div className={getClassNameFor('/projects')}>
+            <div className="header-icon">
+              <i className="fa-solid fa-code"></i>
+            </div>
+            <p>Projects</p>
           </div>
-          <p>Projects</p>
-        </div>
-        <div className="header-section">
-          <div className="header-icon">
-            <i className="fa-solid fa-envelope"></i>
+        </a>
+        <a href="/contact">
+          <div className={getClassNameFor('/contact')}>
+            <div className="header-icon">
+              <i className="fa-solid fa-envelope"></i>
+            </div>
+            <p>Contact</p>
           </div>
-          <p>Contact</p>
-        </div>
+        </a>
       </div>
     </>
   );
